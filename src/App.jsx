cb598d77add1b0cf859cb8df96ca0d540348915b
shiroichi28 <manikandanmk2972@gmail.com>
2023-06-17 21:21:03 +0530

    Intial Commit

---
+++ src/App.jsx
@@ -0,0 +1,68 @@
+import { useState } from "react";
+
+function App() {
+  const [calc, setCalc] = useState("");
+  const [result, setResult] = useState("");
+  const operators = ["/", "*", "+", "-", "."];
+  const updateCalc = (value) => {
+    if (
+      (operators.includes(value) && calc === "") ||
+      (operators.includes(value) && operators.includes(calc.slice(-1)))
+    ) {
+      return;
+    }
+    setCalc(calc + value);
+    if (!operators.includes(value)) {
+      setResult(eval(calc + value).toString());
+    }
+  };
+
+  const createDigits = () => {
+    const digits = [];
+    for (let i = 1; i < 10; i++) {
+      digits.push(
+        <button key={i} onClick={() => updateCalc(i.toString())}>
+          {i}
+        </button>
+      );
+    }
+    return digits;
+  };
+  const calculate = () => {
+    setCalc(eval(calc).toString());
+  };
+  const deleteLast = () => {
+    if (calc == "") {
+      return;
+    }
+    const value = calc.slice(0, -1);
+    setCalc(value);
+  };
+  return (
+    <>
+      <div className="App">
+        <div className="calculator">
+          <div className="display">
+            {result ? <span>({result})</span> : ""}&nbsp;
+            {calc || "0"}
+          </div>
+          <div className="operators">
+            <button onClick={() => updateCalc("/")}>/</button>
+            <button onClick={() => updateCalc("*")}>*</button>
+            <button onClick={() => updateCalc("+")}>+</button>
+            <button onClick={() => updateCalc("-")}>-</button>
+            <button onClick={deleteLast}>Del</button>
+          </div>
+          <div className="digits">
+            {createDigits()}
+            <button onClick={() => updateCalc("0")}>0</button>
+            <button onClick={() => updateCalc(".")}>.</button>
+            <button onClick={calculate}>=</button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
